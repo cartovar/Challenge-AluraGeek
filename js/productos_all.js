3b6f2e26c1599132b1099c8c07fc942219fd86ef
cartovar <ing.tovarada@gmail.com>
2023-03-27 22:13:36 -0400

    Se agrega el Proyecto

---
+++ js/productos_all.js
@@ -0,0 +1,25 @@
+import { listaServices } from "../service/client-service.js";
+//backticks
+const crearNuevaLinea = (imagen, categoria, nombre_prod, precio_prod, id) => {
+    const linea = document.createElement("div");
+    
+    const contenido = `
+    <div class="productos_all">
+    <a class="link_producto" href="./descripcion_productos.html?id=${id}&categoria=${categoria}">
+    <div class="bng_box">
+    <img class="img" src="${imagen}" alt="${nombre_prod}">
+    </div>
+    <div>
+      <ul>
+        <li class="nombre_producto">${nombre_prod}</li>
+        <li class="precio_producto">${precio_prod}</li>
+        <li>Ver producto</li>
+      </ul>   
+    </div>
+    </a>
+ </div>
+    `;
+    linea.innerHTML = contenido;
+  
+    return linea;
+  };
